9b758c98d8016edd362c28d3b09eee06a1b2bada
micaelle <micaelle@mail.ru>
2014-01-26 00:48:27 +0600

    коммит для бранча 1

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace test {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("hello world");
+            Console.WriteLine("hello");
+            Console.WriteLine("world");
         }
     }
 }
